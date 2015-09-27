008e1ba3ec9ba340efbd5a7034473d695c5c6b7f
Elena Petkevich <petkevich.elena@gmail.com>
2015-09-28 02:55:47 +0300

    Added FastBitmap class for basic work with image Added convertion image to grayscale

---
+++ face-direction-recognizer/FastBitmap.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Runtime.InteropServices;
+
+namespace face_direction_recognizer
+{
+    class FastBitmap
+    {
+        private static ColorPalette _palette;
+
+        static FastBitmap()
+        {
+            Bitmap bmp = new Bitmap(1, 1, PixelFormat.Format8bppIndexed);
+            _palette = bmp.Palette;
+            for (int i = 0; i <= 255; i++)
+            {
+                _palette.Entries[i] = Color.FromArgb(i, i, i);
+            }
+        }
+
+        private Bitmap _bitmap;
+        private byte[] _grayPixels;
+
+        public int Height
+        {
+            get;
+            private set;
+        }
+
+        public int Width
+        {
+            get;
+            private set;
+        }
+
+        public Bitmap GrayBitmap
+        {
+            get
+            {
+                Bitmap bitmap = new Bitmap(Width, Height, PixelFormat.Format8bppIndexed);
+                bitmap.Palette = _palette;
+                BitmapData data = bitmap.LockBits(new Rectangle(0, 0, Width, Height), ImageLockMode.WriteOnly, bitmap.PixelFormat);
+                Marshal.Copy(_grayPixels, 0, data.Scan0, _grayPixels.Length);
+                bitmap.UnlockBits(data);
+                return bitmap;
+            }
+        }
+
+        public byte this[int x, int y]
+        {
+            get
+            {
+                return _grayPixels[y * Width + x];
+            }
+            set
+            {
+                _grayPixels[y * Width + x] = value;
+            }
+        }
+
+        public FastBitmap(Bitmap bitmap)
+        {
+            _bitmap = bitmap;
+            _grayPixels = ToGrayscale(_bitmap);
+            Height = _bitmap.Height;
+            Width = _bitmap.Width;
+        }
+
+        private byte[] ToGrayscale(Bitmap bitmap)
+        {
+            int bytesPerChannel = Bitmap.GetPixelFormatSize(bitmap.PixelFormat) / 8;
+            Rectangle rect = new Rectangle(0, 0, bitmap.Width, bitmap.Height);
+            BitmapData colorData = bitmap.LockBits(rect, ImageLockMode.ReadOnly, bitmap.PixelFormat);
+            IntPtr colorPointer = colorData.Scan0;
+            byte[] pixels = new byte[colorData.Height * colorData.Width * bytesPerChannel];
+            byte[] grayPixels = new byte[bitmap.Width * bitmap.Height];
+            Marshal.Copy(colorPointer, pixels, 0, pixels.Length);
+            bitmap.UnlockBits(colorData);
+            int j = 0;
+            for (int i = 0; i < pixels.Length; i += bytesPerChannel)
+            {
+                grayPixels[j++] = (byte)(0.299 * pixels[i + 2] + 0.587 * pixels[i + 1] + 0.114 * pixels[i]);
+            }
+            return grayPixels;
+        }
+    }
+}
--- face-direction-recognizer/Form1.cs
+++ face-direction-recognizer/Form1.cs
@@ -1,20 +1,39 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Drawing;
 using System.Windows.Forms;
+using AForge.Video.DirectShow;
+using AForge.Video;
 
 namespace face_direction_recognizer
 {
     public partial class Form1 : Form
     {
+        private VideoCaptureDevice videoSource;
+
         public Form1()
         {
             InitializeComponent();
+            InitializeCapture();
+        }
+
+        private void InitializeCapture()
+        {
+            FilterInfoCollection videoDevices = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            videoSource = new VideoCaptureDevice(videoDevices[0].MonikerString);
+            videoSource.NewFrame += new NewFrameEventHandler(NewFrameHandler);
+            videoSource.Start();
+        }
+
+        private void NewFrameHandler(object sender, NewFrameEventArgs eventArgs)
+        {
+            Bitmap bitmap = (Bitmap)eventArgs.Frame.Clone();
+            FastBitmap fastBitmap = new FastBitmap(bitmap);
+            pictureBox1.Image = fastBitmap.GrayBitmap;
+        }
+
+        private void OnFormClosed(object sender, FormClosedEventArgs e)
+        {
+            videoSource.Stop();
+            videoSource.WaitForStop();
         }
     }
 }
